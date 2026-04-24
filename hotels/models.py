2dfc1fae7fc5c806eeacbb8c318c0ddcc393aa2e
Angeloperaltama01 <peraltajhun4@gmail.com>
2026-04-24 19:03:00 +0800

    new homepage, rooms and room list

--- hotels/models.py
+++ hotels/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class RoomType(models.Model):
+    room_type = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price_per_night = models.DecimalField(max_digits=10000, decimal_places=2)
+
+
+class Rooms(models.Model):
+    room_number = models.IntegerField(unique=True)
+    category = models.ForeignKey(
+        RoomType, on_delete=models.CASCADE, related_name='category')
+    is_available = models.BooleanField(default=True)
+    reason = models.CharField(max_length=100)
--- hotels/views.py
+++ hotels/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import Rooms, RoomType
+from django.views.generic import ListView
 # Create your views here.
+
+
+class RoomList(ListView):
+    model = Rooms
+    template_name = 'hotels/room_list.html'
+    context_object_name = 'rooms'
+
+    def get_queryset(self):
+        queryset = Rooms.objects.select_related('category').all()
+        return queryset
