06051b7216bc7a5984289801cb1ed2da4745f3cd
Angeloperaltama01 <peraltajhun4@gmail.com>
2026-05-15 15:12:03 +0800

    added booking list

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-
+from . views import BookingList
 app_name = 'bookings'
 urlpatterns = [
     path('booking_form/', views.create_booking, name='booking-form'),
+    path('booking_list/', BookingList.as_view(), name='booking-list'),
     path('booking_payment/<int:pk>', views.payment_page, name='booking-payment'),
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -2,9 +2,47 @@ from .models import Rooms, Bookings  # Ensure these names match your models.py
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from hotels.models import Rooms
+from django.db.models import Q
 from .models import Bookings
 from .forms import BookingForm
+from django.views.generic import ListView
 from django.contrib import messages
+
+
+class BookingList(ListView):
+    model = Bookings
+    template_name = 'bookings/booking_list.html'
+    context_object_name = 'books'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = Bookings.objects.all().order_by('check_in')
+
+        query = self.request.GET.get('q')
+
+        if query:
+
+            queryset = queryset.filter(
+                Q(email__icontains=query) |
+                Q(room__number__icontains=query)
+            )
+        return queryset
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        page_obj = context['page_obj']  # kinukuha nya yung paginated by
+
+        context.update({
+            # para mas madali tawagin ginawang dictonary
+            'current_page': page_obj.number,
+            'last_page': page_obj.paginator.num_pages,
+            'prev_num': page_obj.previous_page_number() if page_obj.has_previous() else None,
+            'next_num': page_obj.next_page_number() if page_obj.has_next() else None,
+            'has_previous': page_obj.has_previous(),
+            'has_next': page_obj.has_next(),
+        })
+        return context
 
 
 def is_room_available(room, check_in, check_out):
